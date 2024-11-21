ba3a3a2b86f5aead2f75300f96eff7ef01f64f56
Jordan Bettencourt <jmbettenc@gmail.com>
2024-11-20 19:27:49 -0800

    Add delegate attest tutorial files

---
+++ src/05-delegate-attest/DelegateAttest.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ISP } from "@ethsign/sign-protocol-evm/src/interfaces/ISP.sol";
+import { Attestation } from "@ethsign/sign-protocol-evm/src/models/Attestation.sol";
+
+contract DelegateAttest is Ownable {
+    ISP public spInstance;
+    uint64 public schemaId;
+
+    constructor() Ownable(_msgSender()) { }
+
+    function setSPInstance(address instance) external onlyOwner {
+        spInstance = ISP(instance);
+    }
+
+    function setSchemaID(uint64 schemaId_) external onlyOwner {
+        schemaId = schemaId_;
+    }
+
+    function createAttestation(Attestation calldata att, string calldata indexingKey, bytes calldata delegationSignature, bytes calldata extraData) external returns (uint64) {
+        uint64 attestationId = spInstance.attest(att, indexingKey, delegationSignature, extraData);
+        return attestationId;
+    }
+}
